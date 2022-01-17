dc4e30f61df540b01c3eec07dce6abeb39a9d0dd
EvelynDevia29 <evelynquevedo817@gmail.com>
2022-01-17 14:59:38 -0500

    creación de las funciones en JS, creacion del formulario HTMl para interacción con JS

--- figuras.js
+++ figuras.js
@@ -1,1 +1,42 @@
-console.log("Hola mundo");
+//Código del cuadrado
+
+function perimetroCuadrado(lado) {
+  return lado * 4;
+}
+
+function areaCuadrado(lado) {
+  return lado * lado;
+}
+
+
+//Código del triángulo
+
+function perimetroTriangulo(lado1, lado2, base) {
+  return lado1 + lado2 + base;
+}
+
+function areaTriangulo(base, altura) {
+  return (base * altura) / 2;
+}
+
+
+
+//Código del circulo
+
+const PI = Math.PI;
+
+function diametroCirculo(radio) {
+  return radio * 2;
+}
+
+function perimetroCirculo(radio) {
+  const diametro = diametroCirculo(radio);
+  return diametro * PI;
+}
+
+function areaCirculo(radio) {
+  return radio * radio * PI;
+} 
+
+//Aui interactuamos con el HTML
+
